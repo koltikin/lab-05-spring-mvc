07de028cf8767f9e33a7c1784cd187acf778cfca
ziya <koltikin210@gmail.com>
2023-09-07 18:14:18 +0300

    initial commit

---
+++ src/main/java/com/cydeo/Lab_05_SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab_05_SpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab_05_SpringMvcApplication.class, args);
+    }
+
+}
